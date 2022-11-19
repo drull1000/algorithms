fe611560dfffc90ca570dfdbc07e8209a4829079
Drull <pcaetanop@protonmail.com>
2022-11-19 10:16:10 -0300

    Adding new file

--- findsum.py
+++ findsum.py
@@ -1,6 +1,7 @@
 #Dado um conjunto S de 1 <= n <= 20 inteiros e um inteiro positivo x. Proponha e implemente um algoritmo que verifica se existe um subconjunto de S cuja soma de seus elementos seja x.
 s = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 x = 10
+a = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 t = [ [-1] * 21 for i in range(21) ]
 #implement array
 def somar(array,n,soma):
@@ -11,11 +12,12 @@ def somar(array,n,soma):
     else:
         if t[n-1][soma] != -1:
             return t[n-1][soma]
-        if t[n-1] > soma:
-            return t[n-1][soma] = somar(n-1, soma)
+        if array[n-1] > soma:
+            return t[n-1][soma] = somar(array, n-1, soma)
         else:
-            return t[n - 1][soma] = somar(n - 1, somar) or somar(n - 1, soma - t[n - 1]);
+            return t[n - 1][soma] = somar(array, n - 1, somar) or somar(array, n - 1, soma - t[n - 1]);
     
-print(somar(20,10))
+#print(somar(20,10))
+
 for c in range(21):
     print(t[c])
