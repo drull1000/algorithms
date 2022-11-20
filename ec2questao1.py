2bf48abeb0d0ed075dce6138cbbb79edbae57031
Drull <pcaetanop@protonmail.com>
2022-11-20 18:48:28 -0300

    Adding file and deleting old ones

--- ec2questao1.py
+++ ec2questao1.py
@@ -1,16 +1,22 @@
-#Dado um conjunto S de 1 <= n <= 20 inteiros e um inteiro positivo x. Proponha e implemente um algoritmo que verifica se existe um subconjunto de S cuja soma de seus elementos seja x.
-s = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-x = 10
-t = [ [-1] * 500 for i in range(500) ]
-def soma(p,n):
-    if p == 0 or n < 0:
-        return 0
-    elif n == 0:
+array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+tabela = [[-1 for i in range(2000)] for j in range(2000)]
+soma = int(input("Digite a soma desejada: "))
+n = 20
+
+def acharSoma(array, n, soma):
+    if soma == 0:
         return 1
+    if n <= 0:
+        return 0
+    if tabela[n - 1][soma] != -1:
+        return tabela[n - 1][soma]
+    if array[n - 1] > soma:
+        tabela[n - 1][soma] = acharSoma(array, n - 1, soma)
+        return tabela[n - 1][soma]
     else:
-        if t[p][n] == -1:
-            t[p][n] = soma(p-1, n-s[p-1])+soma(p-1,n)
-        return t[p][n]
-print(soma(20,7))
-for c in range(21):
-    print(t[c])
+        tabela[n - 1][soma] = acharSoma(array, n - 1, soma)
+        return tabela[n - 1][soma] or acharSoma(array, n - 1, soma - array[n - 1])
+if acharSoma(array, n, soma) == 1:
+    print(f"Existe um subconjunto para a soma {soma}")
+else:
+    print(f"Não existe um subconjunto para a soma {soma}")
