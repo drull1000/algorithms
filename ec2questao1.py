4aa9e66a004b892749c440c8584c13da89101738
Drull <pcaetanop@protonmail.com>
2022-11-24 11:31:53 -0300

    1 subset implemented

--- ec2questao1.py
+++ ec2questao1.py
@@ -1,15 +1,19 @@
 array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-tabela = [[-1 for i in range(100)] for j in range(100)]
-subset = [-1 for i in range(100)] 
+soma = 69
 #soma = int(input("Digite a soma desejada: "))
-soma = 50
-n = 20
+tabela = [[-1 for i in range(soma+1)] for j in range(soma+1)]
+subset = [0 for i in range(soma+1)] 
+printSubset = []
+n = len(array)
+count = 0
 
 def acharSoma(array, n, soma):
     if soma == 0:
         return 1
     elif n <= 0:
         return 0
+    elif soma in array:
+        return 1
     elif tabela[n - 1][soma] != -1:
         return tabela[n - 1][soma]
 
@@ -18,15 +22,16 @@ def acharSoma(array, n, soma):
         return tabela[n - 1][soma]
     else:
         tabela[n - 1][soma] = acharSoma(array, n - 1, soma)
-        # This is interesting. It produces the subset we want, but adds the same number in the sub array.
-        if subset[soma] < n and n not in subset:
-            subset[soma] = n+1
-        print(f"n: {n}")
-        print(f"soma: {soma}")
-        print(f"--------")
+        if n > subset[soma] and n not in subset:
+            subset[soma] = n
         return tabela[n - 1][soma] or acharSoma(array, n - 1, soma - array[n - 1])
 if acharSoma(array, n, soma) == 1:
-    print(f"Existe um subconjunto para a soma {soma}")
+    print(f"Existe um subconjunto para a soma {soma}.")
+    for i in range(len(array)-1, -1, -1):
+        if array[len(array)-1] != 0 and count+array[i] <= soma:
+                count += array[i]
+                printSubset.append(array[i])
+
+    print(f"O subconjunto é: {printSubset}")
 else:
-    print(f"Não existe um subconjunto para a soma {soma}")
-print(subset)
+    print(f"Não existe um subconjunto para a soma {soma}.")
