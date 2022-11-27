0059b03ae81d185bb5b8fc2263b62b1dc5a16bcf
Drull <pcaetanop@protonmail.com>
2022-11-27 10:13:41 -0300

    Working version

--- ec2questao2.py
+++ ec2questao2.py
@@ -1,47 +1,35 @@
-checks = []
-def verifyMessage(n, message, code):
+n = int(input("Qual a quantidade de vezes que o padrão se repete? "))
+message = input("Qual a sua mensagem? ")
+code = input("Qual o código? ")
+if 0 > n > 10000:
+    print("Padrão grande demais. O limite é 10000")
+    exit()
+if 0 > len(message) > 10000:
+    print("Mensagem grande demais. O limite é 10000")
+    exit()
+if 0 > len(code) > 1000:
+    print("Código grande demais. O limite é 1000")
+    exit()
+
+def generateTable(message, code):
     table = [-1 for i in range(len(message))]
-    #loop through the entire message to find it. write a check on the index
-    # mark where the code chars are
-    # loop back going from where they are, ignoring the index with check
-    # until the next char.
-    if n == 0:
-        return 1
     for i in range(len(code)):
         for j in range(len(message)):
-            #create array with code indexes and where they appear
             if code[i] == message[j]:
-                #table[j] = i
                 table[j] = i
-    #remove all the -1's from the array
     table = list(filter((-1).__ne__, table))
+    return table
+table = generateTable(message, code)
 
-    #----------------------------
-    # WE ALREADY HAVE THE ARRAY, WE JUST NEED TO SEE IF THE VALUE IS GOING TO 
-    # DECREASE AND CHANGE THE VALUE OF OUR N AS IT DOES.
-    #----------------------------
-
-    for i in range(len(table)-1): 
-        # if the current item is lesser than the next one
-        # and different from -1
-        #we need to check if the current item is always increasing
-
-        #if table[i] < table[i+1] and (table[i] != -1 and table[i+1] != -1):
-        if table[i+1] != -1:
-            if table[i] < table[i+1]:
-                pass
-            #the pattern is met if the last item we saw is equal to the legth of the code
-            print(table[i])
-            if table[i] == len(code):
-                print("pattern met")
-
-    
-    print("---")
-    print(table)
-    print("---")
-    print(checks)
-
-verifyMessage(2, "eu sou drull", "esdl")
-
-# look for first code in the string.
-# call 
+def verifyMessage(n, table):
+    i = 0
+    while i < len(table)-1:
+        if table[i+1] <= table[i]:
+            table.pop(i+1)
+            n -= 1
+            i = 0
+        i+=1
+    if (n == 0): return 1
+    else: return 0
+if (verifyMessage(n-1,table)): print("Y")
+else: print("N")
